90f536ed4311c40c128f258672c2d6850db435b6
Pavel Oleynikov <Pavlushka19805@gmail.com>
2026-04-19 16:43:22 +0400

    create templates

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -24,6 +25,7 @@ urlpatterns = [
     path("ads/", include("ads.urls")),
     path("users/", include("users.urls")),
     path("feedback/", include("feedback.urls")),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     # Документация
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
